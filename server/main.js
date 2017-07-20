13ae11381d4ecac30728437a73f8e76a54dbadb0
Scott Owen <yodowen@gmail.com>
2017-07-19 22:09:06 -0600

    socket skeleton complete some game logic started

--- server/main.js
+++ server/main.js
@@ -3,8 +3,10 @@ const app = express()
 var server 	= require('http').Server(app);
 var io 		= require('socket.io')(server);
 
-var scanner = require("./scanner.js");
+const totalGameTime = 20000; // in miliseconds
+var prevColor = 1;
 
+//var scanner = require("./scanner.js");
 //app.use(express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/resources'));
 
@@ -14,11 +16,67 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
   console.log('listening on port 3000!')
-})
+});
 
 server.listen(4400);
 
 io.on('connection', function (socket) {
-  console.log("Server Socket connected"); 
+  console.log("Socket Connected")
+  var SerialPort = require('serialport');
+  var Readline = SerialPort.parsers.Readline;
+  var port = new SerialPort('COM3', {
+    autoOpen: false,
+    baudRate: 115200
+  });     
+
+  /** Returns a random integer between min (inclusive) and max (inclusive)
+   * Using Math.round() will give you a non-uniform distribution! */
+  function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function mainGameLoop(){
+    setTimeout(function(){ // start Timer to End Game
+      socket.emit('endOfGame'); 
+      port.close(function(){});
+    }, totalGameTime);
+    //socket.emit("newColorEvent",getRandomInt(1,4) );
+  }
+
   socket.emit('connected', 123);
+  socket.on('startGame', function(data) {
+    //socket.emit("newColorEvent",1);
+   port.open(function(){});
+    mainGameLoop();
+  });
+
+  /* Serial Port Stuff */
+
+
+  port.on('open', function() {
+    console.log("port opened")
+  });
+
+  port.on('data', function (data) {
+    //console.log('Data:', mbRec);
+    var mbRec = new Buffer(data, 'utf-8')
+	  mbRec = mbRec.toString();
+    console.log('Data:', data);
+    console.log('scanned:'+ mbRec+" prevColor:"+prevColor);
+    if(Number.parseInt(mbRec) === prevColor){
+      var newColor = getRandomInt(1,4);
+      prevColor = newColor;
+      console.log('color:', newColor);
+      socket.emit("newColorEvent", newColor);
+    }
+  });
+
+  port.on('close', function (err) {
+    console.log('port closed');
+  });
+
+  port.on('error', function (err) {
+    console.error("error", err);
+  });
+
 });
