e4ec6d647f7226102991e3bf5a972e2f837f9c04
smrtsmrf <smrtsmrf@gmail.com>
2017-07-20 00:29:11 -0600

    clean up server file a bit

--- server/main.js
+++ server/main.js
@@ -1,24 +1,28 @@
-const express = require('express')
-const app = express()
-var server 	= require('http').Server(app);
+const express = require('express');
+const app = express();
+// var server 	= require('http').Server(app);
+var server 	= require('http').createServer(app);
 var io 		= require('socket.io')(server);
 
-var scanner = require("./scanner.js");
+// var scanner = require("./scanner.js");
 
 //app.use(express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/resources'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
-})
+});
 
-app.listen(3000, function () {
-  console.log('listening on port 3000!')
-})
-
-server.listen(4400);
+// app.listen(3000, function () {
+//   console.log('listening on port 3000!')
+// })
 
 io.on('connection', function (socket) {
-  console.log("Server Socket connected"); 
+  console.log("Server Socket connected");
   socket.emit('connected', 123);
 });
+
+server.listen(3000, function() {
+  console.log('socket on port 3000');
+});
+
