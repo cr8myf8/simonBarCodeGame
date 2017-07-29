0b7fe4f6accbf2a22291ba40149bb703403f9518
smrtsmrf <smrtsmrf@gmail.com>
2017-07-29 00:12:42 -0600

    give up on binding new game button to keyboard event

--- lib/src/components/leaderboard/leaderboard_component.dart
+++ lib/src/components/leaderboard/leaderboard_component.dart
@@ -1,6 +1,8 @@
 // Copyright (c) 2017, candice. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:html';
+
 import 'package:angular2/angular2.dart';
 import 'package:angular_components/angular_components.dart';
 import 'package:angular2/router.dart';
